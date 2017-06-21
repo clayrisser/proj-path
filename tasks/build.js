9d4ea1e24f97c5756eb8e0e7eb6bfff0f33a15b2
Jam Risser <jam@jamrizzi.com>
2017-06-21 18:52:27 -0500

    Replaced toolz-cli with do-task

--- tasks/build.js
+++ tasks/build.js
@@ -1,12 +1,12 @@
 import clean from './clean';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
-import { runTool } from 'toolz-cli';
+import { run } from 'do-task';
 
 const $ = gulpLoadPlugins();
 
 export default async function build() {
-  await runTool(clean);
+  await run(clean);
   return new Promise((resolve, reject) => {
     gulp.src(['./src/proj-path.js'])
       // .pipe($.eslint())
@@ -21,7 +21,11 @@ export default async function build() {
         plugins: ['babel-plugin-transform-async-to-generator']
       }))
       .pipe($.uglify().on('error', reject))
-      .pipe(gulp.dest('./dist/'))
+      .pipe($.rename({
+        basename: 'index',
+        extname: '.js'
+      }))
+      .pipe(gulp.dest('./'))
       .on('end', resolve).on('error', reject);
   });
 }
--- tasks/start.js
+++ tasks/start.js
@@ -3,7 +3,7 @@ import projPath from '../src/proj-path';
 
 export default async function start() {
   await new Promise((resolve, reject) => {
-    childProcess.spawn('node ./node_modules/babel-cli/bin/babel-node ./tools/test.js', {
+    childProcess.spawn('node ./node_modules/babel-cli/bin/babel-node ./test/test.js', {
       stdio: 'inherit',
       shell: true
     }).on('close', resolve).on('error', reject);
