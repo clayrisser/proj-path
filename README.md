9bd7cf0fa6218bb8ae36b0e61458c7c9ac134ab2
Jam Risser <jam@jamrizzi.com>
2017-06-21 22:19:46 -0500

    Added gitignore-clean

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # proj-path
 
-Get the project path of your NodeJS application
+Get the project path of your NodeJS application from anywhere
 
 Please &#9733; this repo if you found it useful &#9733; &#9733; &#9733;
 
--- tasks/clean.js
+++ tasks/clean.js
@@ -1,24 +1,8 @@
 import fs from 'fs-extra-promise';
 import path from 'path';
-import _ from 'lodash';
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
 import projPath from '../src/proj-path';
-
-const $ = gulpLoadPlugins();
+import gitignoreClean from 'gitignore-clean';
 
 export default async function clean() {
-  const ignore = [
-    'node_modules/'
-  ];
-  const files = await fs.readFileAsync(path.resolve(projPath(), '.gitignore')).then((file) => {
-    const body = file.toString('utf8');
-    return _.filter(body.replace(/\\/g, '').split('\n'), (line) => {
-      return (line.length > 0 && line[0] !== '#' && !_.includes(ignore, line));
-    });
-  });
-  await new Promise((resolve, reject) => {
-    gulp.src(files, { read: false })
-      .pipe($.clean()).on('finish', resolve);
-  });
+  await gitignoreClean(path.resolve(projPath(), '.gitignore'), ['node_modules/']);
 }
